4fc054e35122d46a733c01ac836679e79a04c717
Shimul Chowdhury <shimulblogs@gmail.com>
2015-01-10 19:22:52 +0600

    Few Basic changes

--- Dev-laravel/app/config/app.php
+++ Dev-laravel/app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'dFhk2CBCy4TU2UuZuBUumVjpYFhFyvwB',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
